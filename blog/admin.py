ffa65fa84f44f70198bbe01622c0ac0232102ec1
DevAmirSaman <dev.amirsaman@gmail.com>
2025-11-18 14:32:26 +0330

    feat(admin): register Post and Tag models

--- blog/admin.py
+++ blog/admin.py
@@ -1,1 +1,31 @@
 from django.contrib import admin
+
+from .models import Post, Tag
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'status', 'published_at', 'created_at')
+    list_filter = (
+        'status',
+        'author',
+        'tags',
+    )
+    search_fields = ('title', 'content', 'author__username')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'published_at'
+    ordering = ('-published_at',)
+    filter_horizontal = ('tags',)
+    readonly_fields = ('created_at', 'updated_at', 'author')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.author = request.user
+
+        super().save_model(request, obj, form, change)
