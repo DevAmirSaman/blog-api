7804983e8c4afdc5cd8a11b1fc5e1788c35fee02
DevAmirSaman <dev.amirsaman@gmail.com>
2025-12-03 00:25:36 +0330

    refactor(models): add default ordering to Tag model

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,9 @@ User = get_user_model()
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
